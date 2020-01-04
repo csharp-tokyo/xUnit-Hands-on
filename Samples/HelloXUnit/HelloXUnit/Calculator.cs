7a1ea215d515b4700462e33bb1bd56cf0c45b9f9
Atsushi Nakamura <nuits.jp@live.jp>
2020-01-05 08:29:14 +0900

    Update Calculator for static class

--- Samples/HelloXUnit/HelloXUnit/Calculator.cs
+++ Samples/HelloXUnit/HelloXUnit/Calculator.cs
@@ -1,6 +1,6 @@
 ﻿namespace HelloXUnit
 {
-    public class Calculator
+    public static class Calculator
     {
         public static int Add(int x, int y) => x + y;
         public static int Subtract(int x, int y) => x - y;
