52b6511fa2d2cd055f8bf9d1141053a0033bb32d
Atsushi Nakamura <nuits.jp@live.jp>
2020-01-05 08:45:36 +0900

    Update Workspace/CapturingOutput

--- WorkSpace/CapturingOutput/CapturingOutput.Tests/UnitTest1.cs
+++ WorkSpace/CapturingOutput/CapturingOutput.Tests/UnitTest1.cs
@@ -5,18 +5,5 @@ namespace CapturingOutput.Tests
 {
     public class UnitTest1
     {
-        private readonly ITestOutputHelper _output;
-
-        public UnitTest1(ITestOutputHelper output)
-        {
-            _output = output;
-            _output.WriteLine("This is output from {0}", "Constructor");
-        }
-
-        [Fact]
-        public void Test1()
-        {
-            _output.WriteLine("This is output from {0}", "Test1");
-        }
     }
 }
